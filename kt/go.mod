be1542b891e93d2bad3826244fef5392050df750
楊芷㚬 <yzj90596@yangzhijunde-MacBook-Air.local>
2024-04-19 00:03:02 +0800

    update: date init

--- kt/go.mod
+++ kt/go.mod
@@ -13,6 +13,7 @@ require (
 )
 
 require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 )
@@ -38,5 +39,6 @@ require (
 	google.golang.org/genproto v0.0.0-20230629202037-9506855d4529 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230629202037-9506855d4529 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/mysql v1.5.6
 	gorm.io/gorm v1.25.9
 )
--- kt/internal/data/data.go
+++ kt/internal/data/data.go
@@ -2,24 +2,43 @@ package data
 
 import (
 	"kt/internal/conf"
+	"time"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 // ProviderSet is data providers.
-var ProviderSet = wire.NewSet(NewData, NewMachineRepo)
+var ProviderSet = wire.NewSet(NewData, NewGormDB, NewMachineRepo)
 
 // Data .
 type Data struct {
 	db *gorm.DB
 }
 
+func NewGormDB(c *conf.Data) (*gorm.DB, error) {
+	dsn := c.Database.Source
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+	sqlDB.SetMaxIdleConns(50)
+	sqlDB.SetMaxOpenConns(150)
+	sqlDB.SetConnMaxLifetime(time.Second * 25)
+	return db, err
+}
+
 // NewData .
-func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
+func NewData(c *conf.Data, logger log.Logger, db *gorm.DB) (*Data, func(), error) {
 	cleanup := func() {
 		log.NewHelper(logger).Info("closing the data resources")
 	}
-	return &Data{}, cleanup, nil
+	return &Data{db: db}, cleanup, nil
 }
